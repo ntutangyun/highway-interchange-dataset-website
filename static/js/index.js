df4d0ad782df259c6fec053221c23176ed3ac698
#TANG YUN# <ntutangyun@gmail.com>
2022-09-26 15:31:37 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,5 @@
 let interchangeClassSelect,
-    interchangeSampleSelect,
     interchangeSampleImage,
-    buttonDownloadNetXML,
-    buttonDownloadPNG,
-    buttonDownloadXodr,
     sampleOriginContainer,
     sampleMapContainer,
     sampleTopologyContainer,
@@ -14,19 +10,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.log(mapPathData);
 
     interchangeClassSelect = document.getElementById("interchange-class-select");
-    interchangeSampleSelect = document.getElementById("interchange-sample-select");
     interchangeSampleImage = document.getElementById("interchange-sample-image");
     classSampleContainer = document.getElementById("class-sample-container");
-
-    buttonDownloadNetXML = document.getElementById("btn-down-net-xml");
-    buttonDownloadPNG = document.getElementById("btn-down-png");
-    buttonDownloadXodr = document.getElementById("btn-down-xodr");
 
     sampleOriginContainer = document.getElementById("sample-origin-container");
     sampleMapContainer = document.getElementById("sample-map-container");
     sampleTopologyContainer = document.getElementById("sample-topology-container");
     sampleSumoContainer = document.getElementById("sample-sumo-container");
-
 
     for (const [classIndex, interchangeFolders] of Object.entries(mapPathData)) {
         const option = document.createElement("option");
@@ -91,53 +81,6 @@ function onSampleButtonRadioClick(sampleIdx) {
     sampleSumoContainer.appendChild(sumoImgDesc);
 }
 
-function toggleDownloadButtonDisable(disabled) {
-    if (disabled) {
-        buttonDownloadNetXML.style.display = "none";
-        buttonDownloadPNG.style.display = "none";
-        buttonDownloadXodr.style.display = "none";
-    } else {
-        buttonDownloadNetXML.style.display = "inline-block";
-        buttonDownloadPNG.style.display = "inline-block";
-        buttonDownloadXodr.style.display = "inline-block";
-    }
-
-    buttonDownloadNetXML.onclick = null;
-    buttonDownloadPNG.onclick = null;
-    buttonDownloadXodr.onclick = null;
-}
-
-// function onInterchangeClassSelect(e) {
-//     interchangeSampleSelect.innerHTML = null;
-//
-//     const defaultSampleOption = document.createElement("option");
-//     defaultSampleOption.value = "0";
-//     defaultSampleOption.textContent = "-";
-//     interchangeSampleSelect.appendChild(defaultSampleOption);
-//
-//     interchangeSampleSelect.disabled = true;
-//     toggleDownloadButtonDisable(true);
-//     interchangeSampleSelect.onchange = null;
-//
-//     const classIndex = e.target.value;
-//     if (classIndex === "0") {
-//         return;
-//     }
-//
-//     const classMaps = mapPathData[classIndex];
-//
-//     for (const [sampleIndex, sampleFolder] of classMaps.entries()) {
-//         const option = document.createElement("option");
-//         option.value = sampleIndex;
-//         option.textContent = sampleIndex + 1;
-//         interchangeSampleSelect.appendChild(option);
-//     }
-//     interchangeSampleSelect.onchange = function (e) {
-//         onInterchangeSampleSelect(classIndex, e.target.value);
-//     };
-//     interchangeSampleSelect.disabled = false;
-// }
-
 function onInterchangeClassSelect(e) {
     classSampleContainer.innerHTML = null;
 
@@ -196,32 +139,3 @@ function onInterchangeClassSelect(e) {
         classSampleContainer.appendChild(sampleContainer);
     }
 }
-
-//
-// function onInterchangeSampleSelect(classIndex, sampleIndex) {
-//     const sampleID = mapPathData[classIndex][sampleIndex];
-//     toggleDownloadButtonDisable(true);
-//
-//     interchangeSampleImage.innerHTML = null;
-//
-//     // const baseURL = `static/maps/${classIndex}/${sampleID}/${sampleID}`;
-//     const baseURL = `https://github.com/ntutangyun/highway-interchange-dataset-website/raw/main/static/4_map/${classIndex}/${sampleID}/${sampleID}`;
-//
-//     const img = document.createElement("img");
-//     img.src = `${baseURL}.png`;
-//     img.style = "width: 100%";
-//     interchangeSampleImage.appendChild(img);
-//
-//     // set download buttons
-//
-//     buttonDownloadNetXML.href = `${baseURL}.net.xml`;
-//     buttonDownloadNetXML.download = `class-${classIndex}-sample-${sampleID}.net.xml`;
-//
-//     buttonDownloadPNG.href = `${baseURL}.png`;
-//     buttonDownloadPNG.download = `class-${classIndex}-sample-${sampleID}.png`;
-//
-//     buttonDownloadXodr.href = `${baseURL}.xodr`;
-//     buttonDownloadXodr.download = `class-${classIndex}-sample-${sampleID}.xodr`;
-//
-//     toggleDownloadButtonDisable(false);
-// }
